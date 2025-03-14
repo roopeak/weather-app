33459e72e4fe776d7784f24a70f99d98e4d9f86e
Roope Kärkkäinen <roopeak@protonmail.com>
2025-03-14 08:27:42 +0200

    Simple fetch weather data function

---
+++ scripts.js
@@ -0,0 +1,17 @@
+async function getWeatherData() {
+	const apiKey = 'WHPCRHSJ4E8JK3YF8LRD3ETR9'
+	const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tampere?key=${apiKey}`;
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const json = await response.json();
+		console.log(json);
+	} catch (error) {
+		console.error(error.message);
+	}
+}
+
+getWeatherData();
